b221db89849d17106821b6d5abafa0a2529095cc
robotenique <julianogarcia_1997@hotmail.com>
2018-11-06 12:58:22 -0500

    initialize development of a undo / redo function

--- instasketch/assets/scripts/sketchbook.js
+++ instasketch/assets/scripts/sketchbook.js
@@ -1,6 +1,10 @@
 /* Javascript code for the Sketchbook view */
 
-var canvas;
+// The canvas of the sketchbook itself
+let canvas;
+// Queue to hold the paths in the canvas (undo + redo functionality)
+let pathQueue = [];
+
 const defaultColor = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00",
                         "#0000FF", "#4B0082", "#9400D3", "#000000"]
 $("#colorpicker > button.color").each(function(index) {
@@ -21,6 +25,7 @@ $("#colorpicker > .clear-sketch").on("click", function (e) {
     canvas.freeDrawingCursor = "crosshair";
     canvas.freeDrawingBrush.color = "red";
     canvas.freeDrawingBrush.width = 10;
+    pathQueue = []
     updateColorButtonBorder($("#colorpicker button:first-child"))
     canvas.renderAll();
 })
@@ -35,9 +40,12 @@ $(function () {
     canvas.renderAll();
 
     document.getElementById('colorpicker').addEventListener('click', function (e) {
-        console.log(e.target.value);
+        //console.log(e.target.value);
         if (e.target.classList.contains("eraser")) {
             canvas.freeDrawingBrush.width = 20;
+            canvas.freeDrawingBrush.color = "#ffffff";
+            updateColorButtonBorder(e.target)
+
         }
         else if(e.target.classList.contains("color")) {
             canvas.freeDrawingBrush.width = 10;
@@ -45,10 +53,45 @@ $(function () {
             canvas.freeDrawingBrush.color = e.target.value;
             canvas.renderAll();
         }
+        else if(e.target.id === "undo"){
+            console.log(pathQueue);
+            const lastPath = pathQueue.shift();
+            //console.dir(lastPath.id);
+            //console.log(lastPath);
+            if (lastPath != undefined) {
+                canvas.getObjects('path').forEach((path) => {
+                    console.log(path.id);
+                    if (path.id === lastPath.id) {
+                        console.log(path.id);
+                    }
+                });
+            }
+        }
 
-        console.log(canvas.freeDrawingBrush.color);
+        //console.log(canvas.freeDrawingBrush.color);
+    });
+    canvas.on("path:created", function (e) {
+        console.log('path:created');
+        console.log(e);
+        e.path.id = fabric.Object.__uid++
+        e.id = e.path.id
+        pathQueue.push(e);
+        console.log("ADICIONADO PATH DE ID: ", e.id)
+        console.log(pathQueue);
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* updateColorButtonBorder: Given a new colorButton, this
    function changes the old button style back to normal,
