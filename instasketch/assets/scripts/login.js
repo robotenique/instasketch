1a35a9def609b75103f6b7f37285ef56b8d55ca3
Sunmin Kim <kmsunmin@gmail.com>
2018-11-06 12:03:06 -0500

    Login JS file for validationForm and verification of teacher or student

---
+++ instasketch/assets/scripts/login.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// User object
+class User {
+  constructor (userName, password) {
+    this.userName = userName;
+    this.password = password;
+  }
+}
+
+// Form validation
+function validateForm() {
+  let userName = $("#uname").val();
+  let password = $("#pw").val();
+  if (userName == "") {
+    alert("Username must be filled out.");
+    return False;
+  }
+  if (password == "") {
+    alsert("Password must be filled out.");
+    return False;
+  }
+}
+
+// When login button is clicked:
+// user object is created (user input: username and password)
+$(document).ready(function() {
+  $("#btn1").click(function() {
+    const userName = $("#uname").val();
+    const password = $("#pw").val();
+    const user = new User(userName, password);
+
+    // Decide if it's student or teacher and
+    // direct the page to teacher/student version accordingly.
+    if (userName == "albus.dumbledore@utoronto.ca" && password == "dumble123") {
+      console.log("Teacher is logging in.");
+    } else if (userName == "student@mail.utoronto.ca" && password =="student123") {
+      console.log("Student is logging in.");
+    }
+
+    console.log(user);
+  });
+});
