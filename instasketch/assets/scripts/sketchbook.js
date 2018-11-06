965713b176b4c11c0c10f75c9c7fbe10c03c0140
robotenique <julianogarcia_1997@hotmail.com>
2018-11-06 17:12:02 -0500

    changing layout of sketchbook, also add dropdown menu

--- instasketch/assets/scripts/sketchbook.js
+++ instasketch/assets/scripts/sketchbook.js
@@ -2,14 +2,15 @@
 
 // The canvas of the sketchbook itself
 let canvas;
-/* Srack to hold the paths in the canvas (undo + redo functionality)
-   MAX items = 30 actions
-*/
+
+/* Stack to hold the paths in the canvas (undo + redo functionality)
+   MAX items = 30 actions */
 let pathStack = [];
 
 
 const defaultColor = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00",
                         "#0000FF", "#4B0082", "#9400D3", "#000000"]
+
 $("#colorpicker > button.color").each(function(index) {
     this.value = defaultColor[index];
     $(this).css("background-color", defaultColor[index]);
@@ -20,21 +21,10 @@ $("#colorpicker button:first-child")
     .css("border-color", "#000000")
     .css("border-width", "3px");
 
-/* Clear Sketch button */
-$("#colorpicker > .clear-sketch").on("click", function (e) {
-    canvas.clear();
-    canvas.backgroundColor = '#ffffff';
-    canvas.isDrawingMode = 1;
-    canvas.freeDrawingCursor = "crosshair";
-    canvas.freeDrawingBrush.color = "red";
-    canvas.freeDrawingBrush.width = 10;
-    pathQueue = []
-    updateColorButtonBorder($("#colorpicker button:first-child"))
-    canvas.renderAll();
-})
+/* Main jquery context */
 
-/* Main canvas: Colors, Tools and other event listeners */
 $(function () {
+    /* ------------ Prepare canvas for the first time ------------ */
     canvas = window._canvas = new fabric.Canvas('canvas');
     canvas.backgroundColor = '#ffffff';
     canvas.isDrawingMode = 1;
@@ -74,22 +64,67 @@ $(function () {
             });
         }
     });
+    /* Redo tool functionality */
     $("#tools").on("click", "#redo", function (e) {
-        const lastPath = pathStack.pop();
-        if (lastPath != undefined) {
-            canvas.getObjects('path').forEach((path) => {
-                if (path.id === lastPath.id) {
-                    canvas.remove(path);
-                }
-            });
+        console.log("Redone!");
+    });
+    /* Clear sketch button functionality */
+    $("#tools").on("click", ".clear-sketch", function (e) {
+        canvas.clear();
+        canvas.backgroundColor = '#ffffff';
+        canvas.isDrawingMode = 1;
+        canvas.freeDrawingCursor = "crosshair";
+        canvas.freeDrawingBrush.color = "red";
+        canvas.freeDrawingBrush.width = 10;
+        // Reset here the thickness slider
+        pathQueue = []
+        updateColorButtonBorder($("#colorpicker button:first-child"))
+        canvas.renderAll();
+
+    });
+    /* ------------ Avaliable sessions Dropdown functionality ------------ */
+    // Get sessions below from server (require server call)
+    const sessions = [{
+        session_id: "1",
+        session_name: "Automobiles"
+    }, {
+        session_id: "2",
+        session_name: "Animals"
+    }];
+
+    let sessionsDropdown = $("#availableSessions");
+    // List of elements in the sessionsDropdown
+    let dropdownList = [];
+    // The element of the dropdown list with the 'active' class
+    let activeSession;
+
+    for (const [idx, s] of sessions.entries()) {
+        const currOption = $("<a>", {
+            'class': `dropdown-item ${idx === 0 ? "active" : ""}`,
+            'href': "javascript:void(0)",
+        }).append(s.session_name);
+        if (idx === 0) {
+            activeSession = currOption;
+            $("#dropdownMenuButton").text(currOption.text());
+        }
+        dropdownList.push(currOption);
+        currOption.appendTo(sessionsDropdown);
+    }
+
+    $(sessionsDropdown).on("click", ".dropdown-item", function () {
+        if (!$(this).hasClass("active")) {
+            $(activeSession).removeClass("active");
+            $(this).addClass("active");
+            activeSession = this;
+            $("#dropdownMenuButton").text($(this).text());
         }
     });
+    $("#submitSketch").on("click", function () {
+        // How to get the original object??
+    })
 });
 
-
-
-
-
+/* AUXILIAR FUNCTIONS */
 /* updateColorButtonBorder: Given a new colorButton, this
    function changes the old button style back to normal,
    and apply the new style to the button passed as argument.
