a11839479ddc87504dbbf3cacc4e0066e947c4d8
Sunmin Kim <kmsunmin@gmail.com>
2018-11-06 12:31:00 -0500

    Teacher username will lead to teacher dashboard. Student username will lead to student dashboard.

--- instasketch/assets/scripts/login.js
+++ instasketch/assets/scripts/login.js
@@ -34,8 +34,14 @@ $(document).ready(function() {
     // direct the page to teacher/student version accordingly.
     if (userName == "albus.dumbledore@utoronto.ca" && password == "dumble123") {
       console.log("Teacher is logging in.");
+      let url = "file:///Users/pigletwithcurls/Desktop/uoft/csc309/ph1_my/instasketch/teacherIndex.html";
+      window.location.href = url;
     } else if (userName == "student@mail.utoronto.ca" && password =="student123") {
       console.log("Student is logging in.");
+      let url = "file:///Users/pigletwithcurls/Desktop/uoft/csc309/ph1_my/instasketch/studentIndex.html";
+      window.location.href = url;
+    } else {
+      alert("Please enter correct username and password.");
     }
 
     console.log(user);
