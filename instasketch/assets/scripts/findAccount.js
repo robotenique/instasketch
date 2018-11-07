a2c8244e3b08a8a338d881e879803c46c02a71a8
Sunmin Kim <kmsunmin@gmail.com>
2018-11-07 01:52:24 -0500

    findAccount to say 'password has been sent'

---
+++ instasketch/assets/scripts/findAccount.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById("search");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span>(x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(e) {
+  if(e.target == modal) {
+    modal.style.display = "none";
+  }
+}
