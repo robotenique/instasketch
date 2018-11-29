e996bf87358e22b82768a09cf910ed6ade6e2a20
Spyguy001 <amritprabh98@hotmail.com>
2018-11-29 14:29:19 -0500

    Fix comma errors in models

--- instasketch/models/session.js
+++ instasketch/models/session.js
@@ -3,22 +3,22 @@ const mongoose = require('mongoose')
 const Session = mongoose.model('Session', {
 	session_id: {
 		type: Number
-	}
+	},
 	teacher_id: {
 		type: ObjectID
-	}
+	},
 	title: {
 		type: String
-	}
+	},
 	date: {
 		type: Date
-	}
+	},
 	marked_submissions: {
 		type: Number
-	}
+	},
 	total_submissions: {
 		type: Number
-	}
+	},
 	open: {
 		type: Boolean
 	}
--- instasketch/models/submission.js
+++ instasketch/models/submission.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const Submission = mongoose.model('Submission', {
 	session_id: {
 		type: ObjectID
-	}
+	},
 	drawing_id: {
 		type: ObjectID
-	}
+	},
 	comments: {
 		type: String
 	}
