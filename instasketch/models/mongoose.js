b0b0e2829b7a4abe60be3a1507e4a1c257527543
robotenique <julianogarcia_1997@hotmail.com>
2018-11-30 00:15:13 -0500

    fix mongoose uri

--- instasketch/models/mongoose.js
+++ instasketch/models/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const mongoURI = 'mongodb://team07:instasketch-07@instasketch-shard-00-00-zrvm1.mongodb.net:27017,instasketch-shard-00-01-zrvm1.mongodb.net:27017,instasketch-shard-00-02-zrvm1.mongodb.net:27017/test?ssl=true&replicaSet=instasketch-shard-0&authSource=admin&retryWrites=true';
 
 // You need to install mongodb on your PC!
-mongoose.connect("mongodb://localhost:27017/instasketch", {
+mongoose.connect(mongoURI, {
     useNewUrlParser: true
 });
 
