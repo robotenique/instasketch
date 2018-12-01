1712bbdde1d01d554c22741194de7dd7f2d23450
Sunmin Kim <kmsunmin@gmail.com>
2018-11-30 20:29:38 -0500

    Confusion by two assets folder

--- instasketch/public/assets/scripts/registration.js
+++ instasketch/public/assets/scripts/registration.js
@@ -7,7 +7,36 @@ const tcode_display = tcode.style.display;
 const teachersList = document.querySelector("#teachers");
 const teachersList_display = teachersList.style.display;
 
-/* User account */
+$.getJSON('../studentProfile/teachers', function(teachers_list) {
+  const teacherList = [];
+  if (teachers_list.length !== 0) {
+    for (const teacher of teachers_list.result) {
+      // console.log(teacher);
+      teacherList.push({
+        first_name: teacher.first_name,
+        last_name: teacher.last_name,
+        school: teacher.school,
+        teacher_id: teacher._id,
+        teacher_code: teacher.teacher_code,
+        email: teacher.email,
+        password: teacher.password,
+        province: teacher.province,
+        path: teacher.path
+      });
+    }
+  }
+  console.log(teacherList);
+  for (const [idx, t] of teacherList.entries()) {
+    const currOption = $("<option>", {
+      'value': t.first_name + ' ' + t.last_name,
+      'id': t.teacher_id,
+    }).append(t.first_name + ' ' + t.last_name);
+    currOption.appendTo(teachersList);
+  }
+});
+
+
+// User account
 class UserAccount {
   constructor (email, password, firstName, lastName, province, school, position, teachercode, teacher) {
     this.email = email;
@@ -17,12 +46,14 @@ class UserAccount {
     this.province = province;
     this.school = school;
     this.position = position;
-    this.teachercode = "";
-    this.teacher = "";
+    this.teacher_id = "";
+    this.teacher_code = "";
   }
 }
 
-// By default, the teacher code input shouldn't appear
+/* Appearance of options available to student or teacher.
+ * By default, the teacher code input shouldn't appear
+ */
 tcode.style.display = "none";
 let prev = null;
 for(let i = 0; i < radioButtons.length; i++) {
@@ -42,8 +73,10 @@ for(let i = 0; i < radioButtons.length; i++) {
     };
 }
 
+/* AJAX */
 $(document).ready(function() {
-  $("#btn1").click(function() {
+  $("#btn1").click(function(e) {
+    e.preventDefault();
     const email = $("#email").val();
     const password = $("#pw").val();
     const firstName = $("#firstName").val();
@@ -51,13 +84,10 @@ $(document).ready(function() {
     const province = $("#province").val();
     const school = $("#school").val();
     const position = "TBA";
-    const teacher = "TBA";
-    const teacherCode = "TBA";
-
-    /** Supposed to create a new UserAccount object and add it
-      * onto the database on the server. This is a mock example.
-      */
-    const account = new UserAccount(email, password, firstName, lastName, province, school, position, teacherCode, teacher);
+    const teacher_id = "TBA";
+    const teacher_code = "TBA";
+    // new account
+    const account = new UserAccount(email, password, firstName, lastName, province, school, position, teacher_id, teacher_code);
 
     // Get value of the radio button for teacher or student
     for (let i = 0; i < radioButtons.length; i++) {
@@ -66,21 +96,48 @@ $(document).ready(function() {
       }
     }
 
-    if (account.position == "teacher") {
-      alert('Registered Teacher');
+    if (account.position === "teacher") {
+      console.log('Registered Teacher');
       const teacherCodeVal = $("#teachercode").val();
-      account.teacherCode = teacherCodeVal;
+      account.teacher_code = teacherCodeVal;
       account.teacher = "None";
-    } else if (account.position == "student") {
-      alert('Registered Student');
-      const teacherVal = $("#teachers").val();
-      account.teacher = teacherVal;
-      account.teacherCode = "None";
+      account.teacher_id = teacher_id;
+
+      $.ajax({
+        type: "post",
+        url: "/registration",
+        data: account,
+        'content-type': 'application/json',
+        success: function(response) {
+          console.log("Success creating a teacher object");
+          window.location.href = "/login";
+        },
+        error: function(response) {
+          alert("Error creating the teacher");
+        }
+      });
+
+    } else if (account.position === "student") {
+      console.log('Registered Student');
+      const teacher_id = $("#teachers").children(":selected").attr("id");
+      account.teacher_code = "None";
+      account.teacher_id = teacher_id;
+
+      $.ajax({
+        type: "post",
+        url: "/registration",
+        data: account,
+        'content-type': 'application/json',
+        success: function(response) {
+          console.log("Success creating a student object");
+          window.location.href = "/login";
+        },
+        error: function(response) {
+          alert("Error creating the student");
+        }
+      });
     }
     // Here we would send the new account to the server
     console.log(account);
-
-    /* Mock example of going back to index page after registration.*/
-    window.location.href = "index.html";
   });
 });
