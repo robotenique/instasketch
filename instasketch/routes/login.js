1f7c20f45ab2305b4f691b97d1a6c950366b18c3
Sunmin Kim <kmsunmin@gmail.com>
2018-12-01 00:18:57 -0500

    Trying sessionChecker on login.js for now

--- instasketch/routes/login.js
+++ instasketch/routes/login.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const session = require('express-session');
 
-router.get('/', (req, res) => {
+const sessionChecker = (req, res, next) => {
+  if (req.session.teacher_id) {
+    res.redirect('/sketchbook');
+  } else if (req.session.teacher_code) {
+    res.redirect('/admin');
+  } else {
+    next();
+  }
+}
 
+router.get('/', sessionChecker, (req, res) => {
   res.render("login", {layout: 'loginLayout', title: 'Instasketch - login'});
 });
 
