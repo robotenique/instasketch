67b02d3125c1af5a71825a33b06e1fa996c36b1e
Sunmin Kim <kmsunmin@gmail.com>
2018-11-30 22:42:24 -0500

    added validator to Teacher object

--- instasketch/models/teacher.js
+++ instasketch/models/teacher.js
@@ -1,34 +1,57 @@
 const mongoose = require('mongoose')
+const validator = require('validator');
 
-const Teacher = mongoose.model('Teacher', {
+const TeacherSchema = new mongoose.Schema({
 	first_name: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
 	},
 	last_name: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
 	},
 	school: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
 	},
 	teacher_id: {
-		type: mongoose.Schema.Types.ObjectId
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		minlength: 1,
 	},
 	teacher_code: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
 	},
 	email: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
+		validate: {
+			validator: validator.isEmail,
+			message: 'Not valid email.'
+		}
 	},
 	password: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 6,
 	},
 	province: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
 	},
 	path: {
 		type: String
 	}
 
-})
+});
+
+const Teacher = mongoose.model('Teacher', TeacherSchema);
 
 module.exports = { Teacher }
