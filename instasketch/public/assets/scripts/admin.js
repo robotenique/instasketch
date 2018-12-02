aa46937ef6692ab66b9db10b6f80e32e0c71d7bf
Sunmin Kim <kmsunmin@gmail.com>
2018-12-02 15:22:49 -0500

    admin can remove students from database

--- instasketch/public/assets/scripts/admin.js
+++ instasketch/public/assets/scripts/admin.js
@@ -74,15 +74,28 @@ $.getJSON('../teacher-profile/students', function(students_list) {
       $("<td>").text(s.last_name),
       $("<td>").text(s.first_name),
       $("<td>").append(
-        $("<button>").text('Add'),
-        $("<button>").text('Modify'),
-        $("<button>").text('Remove')
+        $("<button>", {
+          click: addStudent,
+          text: 'Add',
+          class: 'add',
+        }),
+        $("<button>", {
+          click: modifyStudent,
+          text: 'Modify',
+          class: 'modify',
+        }),
+        $("<button>", {
+          click: removeStudent,
+          text: 'Remove',
+          class: 'remove'
+        })
       )).appendTo(studentTable);
   }
 });
 
 function addStudent(e) {
   e.preventDefault();
+  console.log(e.target.parentElement);
   if (e.target.className === "add") {
     // Find the row that the button is existing.
     console.log("new student");
@@ -91,52 +104,13 @@ function addStudent(e) {
   }
 }
 
-/** After adding student, see a new student object added
-  * Need to use backend later to update the student database
-  * accordingly after adding a new student.
-  */
-if (document.referrer === "addStudent.html") {
-  /* Pretend a new student object added */
-  //  const newStudent = new Student("123", "newStudent", "Hello");
-  //  addStudentToTable(newStudent);
-  alert("back from adding");
-}
-
-/* Add students onto the table after retrieving data from  */
-function addStudentToTable(student) {
-  const studentTable = document.querySelector("#studentTable");
-  const tableRow = document.createElement("tr");
-  const tableId= document.createElement("td");
-  const tableLastName = document.createElement("td");
-  const tableFirstName = document.createElement("td");
-  const tableButton = document.createElement("td");
-  const modifyBtn = document.createElement("button");
-  const removeBtn = document.createElement("button");
-  modifyBtn.className = "modify";
-  removeBtn.className = "remove";
-  modifyBtn.appendChild(document.createTextNode("Modify"));
-  removeBtn.appendChild(document.createTextNode("Remove"));
-  tableId.appendChild(document.createTextNode(student.id));
-  tableLastName.appendChild(document.createTextNode(student.lastName));
-  tableFirstName.appendChild(document.createTextNode(student.firstName));
-  tableButton.appendChild(modifyBtn);
-  tableButton.appendChild(removeBtn);
-  tableRow.appendChild(tableId);
-  tableRow.appendChild(tableLastName);
-  tableRow.appendChild(tableFirstName);
-  tableRow.appendChild(tableButton);
-  studentTable.appendChild(tableRow);
-  studentOnRecord[student.id] = student;
-  numberOfStudents += 1;
-}
-
 /* Direct admin to the modifyStudent.html */
 function modifyStudent(e) {
   e.preventDefault();
   if (e.target.className === "modify") {
     console.log("modify");
     // Directs to the page where admin can modify student.
-    window.location.href = "modifyStudent.html";
+    window.location.href = "/student-profile";
   }
 }
 
@@ -145,13 +119,21 @@ function modifyStudent(e) {
 function removeStudent(e) {
   e.preventDefault();
   // Find if the remove button was clicked
+  const currentRow = e.target.parentElement.parentElement;
+  const removingID = currentRow.firstChild.textContent;
   if (e.target.className === "remove") {
-    // find a row that the button was clicked on
-    const studentTable = document.querySelector("#studentTable");
-    const currRow = e.target.parentElement.parentElement;
-    studentTable.removeChild(currRow);
-    // Remove student from the studentOnRecord list
-    delete studentOnRecord[currRow.childNodes[0].textContent];
-    console.log(studentOnRecord);
+    // AJAX POST to remove the student from database
+    $.ajax({
+      type: "delete",
+      url: "/admin/remove/" + removingID,
+      // data: {id: removingID},
+      success: function(response) {
+        console.log("SUCCESS REMOVING A STUDENT");
+        window.location.href="/admin";
+      },
+      error: function(response) {
+        console.log("PROBLEM REMOVING A STUDENT");
+      }
+    });
   }
 }
