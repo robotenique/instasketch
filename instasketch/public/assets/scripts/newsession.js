30a1985155b49bd59be3bb8174d51664cef9a193
maxboyko <maxboyko@mail.utoronto.ca>
2018-12-04 17:12:47 -0500

    Integrated My Sessions front end with back end

--- instasketch/public/assets/scripts/newsession.js
+++ instasketch/public/assets/scripts/newsession.js
@@ -1,37 +1,52 @@
 'use strict';
+const log = console.log;
 
-/* In Phase 2 this will retrieve passed in data from My Sessions, for now we
- * will use hardcoded values */
-// const windowURL = new URL(window.location.href);
-// const sessionId = windowURL.searchParams.get('id');
-// const sessionTitle = windowURL.searchParams.get('title');
-const sessionId = '106';
-const sessionTitle = 'Planes';
+/* Retrieve the new session's ID */
+const windowURL = new URL(window.location.href);
+const sessionId = windowURL.searchParams.get('id');
 
-/* Variable to access session info container */
-const sessionInfo = document.querySelector('#sessionInfo');
+/* Use the ID to retrieve the title of the session */
+let sessionTitle = '';
+const request = new Request('/sessions/' + sessionId, { method: 'get' });
 
-/* Create session info elements */
-const titleHeader = document.createElement('h1');
-const titleStrong = document.createElement('strong');
-titleStrong.appendChild(document.createTextNode('Session: '));
-titleHeader.appendChild(titleStrong);
-titleHeader.appendChild(document.createTextNode(sessionTitle));
+fetch(request).then((res) => {
+    if (res.status === 200) {
+        return res.json();
+    } else {
+        alert(`Could not retrieve session with id ${sessionId}`);
+    }
+}).then((json) => {
+    sessionTitle = json.result['title'];
 
-const idHeader = document.createElement('h2');
-const idStrong = document.createElement('strong');
-idStrong.appendChild(document.createTextNode('ID: '))
-idHeader.appendChild(idStrong);
-idHeader.appendChild(document.createTextNode(sessionId));
+    /* Variable to access session info container */
+    const sessionInfo = document.querySelector('#sessionInfo');
 
-const instructionsDiv = document.createElement('div');
-instructionsDiv.id = 'instructions';
-const instructions = document.createElement('p');
-const instructionsText = 'Use this ID to submit your drawings to this session!';
-instructions.appendChild(document.createTextNode(instructionsText));
-instructionsDiv.appendChild(instructions);
+    /* Create session info elements */
+    const titleHeader = document.createElement('h1');
+    const titleStrong = document.createElement('strong');
+    titleStrong.appendChild(document.createTextNode('Session: '));
+    titleHeader.appendChild(titleStrong);
+    titleHeader.appendChild(document.createTextNode(sessionTitle));
 
-/* Add session info elements */
-sessionInfo.appendChild(titleHeader);
-sessionInfo.appendChild(idHeader);
-sessionInfo.appendChild(instructionsDiv);
+    const idHeader = document.createElement('h2');
+    const idStrong = document.createElement('strong');
+    idStrong.appendChild(document.createTextNode('ID: '))
+    idHeader.appendChild(idStrong);
+    idHeader.appendChild(document.createTextNode(sessionId));
+
+    const instructionsDiv = document.createElement('div');
+    instructionsDiv.id = 'instructions';
+    const instructions = document.createElement('p');
+    const instructionsText = 'Use this ID to submit your drawings to this session!';
+    instructions.appendChild(document.createTextNode(instructionsText));
+    instructionsDiv.appendChild(instructions);
+
+    /* Add session info elements */
+    sessionInfo.appendChild(titleHeader);
+    sessionInfo.appendChild(idHeader);
+    sessionInfo.appendChild(instructionsDiv);
+
+}).catch((error) => {
+    log(error);
+})
+
