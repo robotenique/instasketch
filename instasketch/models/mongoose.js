beae9f513d6ff0a098eab96e77f26f06980fa1de
robotenique <julianogarcia_1997@hotmail.com>
2018-12-06 12:29:38 -0500

    go back to local mongoose

--- instasketch/models/mongoose.js
+++ instasketch/models/mongoose.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
-//const mongoURI = 'mongodb://team07:instasketch-07@instasketch-shard-00-00-zrvm1.mongodb.net:27017,instasketch-shard-00-01-zrvm1.mongodb.net:27017,instasketch-shard-00-02-zrvm1.mongodb.net:27017/test?ssl=true&replicaSet=instasketch-shard-0&authSource=admin&retryWrites=true';
+const mongoURI = 'mongodb://team07:instasketch-07@instasketch-shard-00-00-zrvm1.mongodb.net:27017,instasketch-shard-00-01-zrvm1.mongodb.net:27017,instasketch-shard-00-02-zrvm1.mongodb.net:27017/test?ssl=true&replicaSet=instasketch-shard-0&authSource=admin&retryWrites=true';
 // "mongodb://localhost:27017/instasketch"
-
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/instasketch';
-
-mongoose.connect("mongodb://localhost:27017/instasketch", {
+// You need to install mongodb on your PC!
+mongoose.connect(mongoURI, {
     useNewUrlParser: true
 });
 
